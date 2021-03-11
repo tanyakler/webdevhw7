bfbd2067c8233c66dbed45dd8452dd3ca6a96840
tanyakler <tkler1919@gmail.com>
2021-03-10 23:57:17 -0500

    end of day tuesday

--- events/priv/repo/migrations/20210303050508_create_posts.exs
+++ events/priv/repo/migrations/20210303050508_create_posts.exs
@@ -4,7 +4,7 @@ defmodule Events.Repo.Migrations.CreatePosts do
   def change do
     create table(:posts) do
       add :name, :text, null: false
-      add :date, :text, null: false
+      add :date, :utc_datetime, null: false
       add :description, :text, null: false
       add :user_id, references(:users), null: false
 
